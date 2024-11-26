e64a7a0162791cdda428fa4ca8afb70f8d62ec4a
Gabriel Souza Fonseca <souzafonseca.gsf@gmail.com>
2024-11-25 22:24:40 -0300

    Update 07-publicar_mensagem.py

--- 07-publicar_mensagem.py
+++ 07-publicar_mensagem.py
@@ -3,18 +3,33 @@ import json
 import logger
 
 class SnsWrapper:
-    """Encapsulates Amazon SNS topic and subscription functions."""
 
-    def __init__(self, sns_resource):
-        """
-        :param sns_resource: A Boto3 Amazon SNS resource.
-        """
+    '''Iniciando o sns e obtendo o sns_resource'''
+    def __init__(self,sns_resource):
         self.sns_resource = sns_resource
 
-    @staticmethod
+    '''Criando um tópico'''
+    def create_topic(self, name):
+        topic = self.sns_resource.create_topic(Name=name)
+        return topic
+
+    '''Criando assinatura e-mail'''
+    def subscribe(self, topic_arn, protocol, endpoint):
+        topic = self.sns_resource.Topic(topic_arn)
+        subscription = topic.subscribe(
+            Protocol=protocol, Endpoint=endpoint, ReturnSubscriptionArn=True
+        )
+        return subscription
+
+    def add_email_subscription(self, topic_arn, email):
+        subscription = self.subscribe(topic_arn, protocol="email", endpoint=email)
+        return subscription
+
+    '''publicando mensagem'''
     def publish_multi_message(
-        topic, subject, default_message, sms_message, email_message
+        self,topic_arn, subject, default_message, sms_message, email_message
     ):
+        topic = self.sns_resource.Topic(topic_arn)
         message = {
             "default": default_message,
             "sms": sms_message,
@@ -24,10 +39,15 @@ class SnsWrapper:
             Message=json.dumps(message), Subject=subject, MessageStructure="json"
         )
         message_id = response["MessageId"]
-        logger.info("Published multi-format message to topic %s.", topic.arn)
-
         return message_id
 
-sns_resource = boto3.resource('sns', region_name='us-east-1')
-SnsWrapper(sns_resource).publish_multi_message("ada","mensagem da aula","x","y","z")
 
+
+sns_resource = boto3.resource('sns', region_name='us-east-1')
+topic_arn = "arn:aws:sns:us-east-1:862147242199:ada"
+
+#Cadastrar um e-mail e confirmar (código comentado pois o e-mail já foi confirmado)
+# email = "souzafonseca.gsf@gmail.com"
+# SnsWrapper(sns_resource).add_email_subscription(topic_arn, email)
+
+SnsWrapper(sns_resource).publish_multi_message(topic_arn,"mensagem da aula","","","mensagem de e-mail enviada com sucesso")
